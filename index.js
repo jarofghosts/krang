6d15565dad65079c296a9e20c7654fc20d75659f
jesse keane <me@jessekeane.me>
2013-09-29 14:13:09 -0700

    setup stuffs

---
+++ index.js
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+
+var c_proc = require('child_process'),
+    util = require('util'),
+    path = require('path'),
+    EE = require('events').EventEmitter
+
+function Krang() {
+  this.configuration = this.load_config()
+
+  return this
+}
+
+util.inherits(Krang, EE)
+
+Krang.prototype.load_config = function () {
+  try {
+    return require(path.join(process.cwd(), '.krang.json'))
+  } catch (e) {
+    return {}
+  }
+}
