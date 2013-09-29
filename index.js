af67b90e3df0915770cfcbb6dd3459863cfc37c3
jesse keane <me@jessekeane.me>
2013-09-29 15:01:43 -0700

    hrmm

--- index.js
+++ index.js
@@ -3,11 +3,20 @@
 var c_proc = require('child_process'),
     util = require('util'),
     path = require('path'),
-    argv = require('optimist')
-    EE = require('events').EventEmitter
+    EE = require('events').EventEmitter,
+    is_cli = (require.main === module)
 
-function Krang() {
-  this.configuration = this.load_config()
+function load_config(dir) {
+  try {
+    return require(path.resolve(dir, '.krang.json'))
+  } catch (e) {
+    return {}
+  }
+}
+
+function Krang(dir) {
+  this.dir = dir || process.cwd()
+  this.config = load_config(this.dir)
   this.static_processes = []
   this.dynamic_processes = []
 
@@ -16,12 +25,18 @@ function Krang() {
 
 util.inherits(Krang, EE)
 
-Krang.prototype.load_config = function () {
-  try {
-    return require(path.join(process.cwd(), '.krang.json'))
-  } catch (e) {
-    return {}
-  }
+Krang.prototype.start = function () {
+  this.emit('started')
 }
 
-Krang.prototype.start
+Krang.prototype.stop = function () {
+  this.emit('stopped')
+}
+
+Krang.prototype.restart = function () {
+  this.once('stopped', this.start.bind(this))
+}
+
+if (is_cli) {
+  var argv = require('optimist').argv
+}
