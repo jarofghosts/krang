84529923ce6732ec28ba68414e4d7b25506cc06c
jesse keane <me@jessekeane.me>
2013-09-29 17:06:59 -0700

    moving right along

--- index.js
+++ index.js
@@ -19,6 +19,9 @@ function Krang(dir) {
   this.config = load_config(this.dir)
   this.static_processes = []
   this.dynamic_processes = []
+  this.current_environment = this.config.environment || {}
+  this.name = this.config.name || this.dir
+  this.running = false
 
   return this
 }
@@ -26,8 +29,28 @@ function Krang(dir) {
 util.inherits(Krang, EE)
 
 Krang.prototype.start = function () {
-  
+  if (this.running) return this.error('Already running')
+  this.running = true
+  for (var i = 0, l = this.config.processes.length; i < l; ++i) {
+    var this_process = this.config.processes[i],
+        command = this_process.command.split(' '),
+        type = this_process.type || 'run_once'
+        cmd_options = {}
+    cmd_options.env = this.current_environment
+    this_process.working_dir && cmd_options.cwd = this_process.working_dir
+    this_process.user_id && cmd_options.uid = this_process.user_id
+    this_process.group_id && cmd_options.gid = this_process.group_id
+
+    var spawned = spawn(command[0], command.splice(1), cmd_options)
+    if (type == 'static' || type == 'dynamic') this.register(this_process.name || this_process.command, type, spawned)
+  }
   this.emit('started')
+}
+
+Krang.prototype.register_process = function (process_name, 
+
+Krang.prototype.error = function (err_string) {
+  this.emit('error', new Error(err_string))
 }
 
 Krang.prototype.refresh = function () {
@@ -37,6 +60,8 @@ Krang.prototype.refresh = function () {
 Krang.prototype.stop = function () {
   var all_processes = this.static_processes.concat(this.dynamic_processes),
       countdown = all_processes.length
+
+  this.once('stopped', function () { this.running = false }.bind(this))
 
   for (var i = 0; i < countdown; ++i) {
     this.kill_process(all_processes[i], count_kill.bind(this))
